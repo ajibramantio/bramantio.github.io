edca1e56485fd6f338ed4b09829dacf78261500f
Aji_Bram <bramantioaji@gmail.com>
2018-09-06 14:43:51 +0700

    Story1 1.0

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from datetime import datetime, date
 # Enter your name here
-mhs_name = '' # TODO Implement this
+mhs_name = 'Bramantio Krisno Aji' # TODO Implement this
 curr_year = int(datetime.now().strftime("%Y"))
-birth_date = date() #TODO Implement this, format (Year, Month, Date)
-npm = None # TODO Implement this
+birth_date = date(1999,08,06) #TODO Implement this, format (Year, Month, Date)
+npm = 1706024495 # TODO Implement this
 # Create your views here.
 def index(request):
     response = {'name': mhs_name, 'age': calculate_age(birth_date.year), 'npm': npm}
