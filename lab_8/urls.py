99444c2e56446a5544905cc90780aec3cca73ff8
Aji_Bram <bramantioaji@gmail.com>
2018-11-16 14:16:09 +0700

    lab9 1.0 + challenge

--- lab_8/urls.py
+++ lab_8/urls.py
@@ -5,4 +5,5 @@ from lab_8 import views as lab_8views
 #url for app
 urlpatterns = [
     re_path(r'^$', lab_8views.index),
+    re_path('data', lab_8views.buku, name="buku"),
 ]
--- lab_8/views.py
+++ lab_8/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.http import HttpResponse
+import urllib.request
+import json
+import requests
 # Create your views here.
 
 response = {}
 def index(request):
     return render(request, 'home.html', response)
+
+# def book_data(request):
+#     try:
+#         search = request.GET["search"]
+#     except:
+#         search = "quilting"
+#     raw_data = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + search).json()
+#     itemss = []
+#     for info in raw_data['items']:
+#         data = {}
+#         data['title'] = info['volumeInfo']['title']
+#         data['author'] = ", ".join(info['volumeInfo']['authors'])
+#         data['publishedDate'] = info['volumeInfo']['publishedDate']
+#         itemss.append(data)
+#     return JsonResponse({"data": itemss})
+
+
+def buku(request):
+    try:
+        search = request.GET["search"]
+    except:
+        search = "quilting"
+
+    getBooksJson = requests.get("https://www.googleapis.com/books/v1/volumes?q=" + search)
+    jsonParsed = json.dumps(getBooksJson.json())
+    return HttpResponse(jsonParsed)
